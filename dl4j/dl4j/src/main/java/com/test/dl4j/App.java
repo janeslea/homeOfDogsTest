69095533073f44b431634c5fc7355dbdea70e504
Janeslea <yaolinzhenwork@163.com>
2018-07-18 00:57:42 +0800

    受限玻尔兹曼机基础教程

---
+++ dl4j/dl4j/src/main/java/com/test/dl4j/App.java
@@ -0,0 +1,13 @@
+package com.test.dl4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
